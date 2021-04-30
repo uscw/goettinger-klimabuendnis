2bba12d10f98f802863e8bf75420a840f2db56e2
uschwar1 <uschwar1@gwdg.de>
2021-04-30 19:29:01 +0200

    repare public

--- bin/docker_reload.sh
+++ bin/docker_reload.sh
@@ -1,8 +1,12 @@
+cd /home/gkb_user/goettinger-klimabuendnis
+
 git pull origin master
+
+/home/gkb_user/bin/hugo
 
 rm -r /home/gkb_user/Docker/nginx-alpine/public/*
 cp -a /home/gkb_user/goettinger-klimabuendnis/public/* /home/gkb_user/Docker/nginx-alpine/public
 
 cd /home/gkb_user/Docker/nginx-alpine/
 docker build . -t goettinger-klimabuendnis
-docker run -d --name goettinger-klimabuendnis goettinger-klimabuendnis
+docker run -d --name goettinger-klimabuendnis -p 80:80 goettinger-klimabuendnis
