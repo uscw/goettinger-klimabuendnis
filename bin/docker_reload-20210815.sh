28483d5e12b2209227c50a2893e250826a0ffae7
uschwar1 <uschwar1@gwdg.de>
2021-08-15 15:27:12 +0200

    Aktualisierung des Web-Inhaltes

--- bin/docker_reload-20210815.sh
+++ bin/docker_reload-20210815.sh
@@ -1,22 +1,18 @@
 UDIR="/home/gkb_user"
 PDIR=${UDIR}/"goettinger-klimabuendnis"
-DDIR=${UDIR}/"/Docker/nginx-latest"
+DDIR=${UDIR}/"/Docker/nginx-alpine"
 HUGO=${UDIR}"/bin/hugo"
 TAG="goettinger-klimabuendnis"
 
 
 cd ${PDIR}
-rm -r public
-git checkout . # to remove all local changes (which are never authorative)
 git pull origin master
-${HUGO}
 
 rm -r ${DDIR}/public
 cp -a public ${DDIR}
-cp ${DDIR}/public/404.html ${DDIR}/public/50x.html # ssl error page also needed
 cd ${DDIR}
 
 docker build . -t ${TAG}
 docker stop  ${TAG}
-docker run -d --name ${TAG} -p 80:80 -p 443:443 --rm ${TAG}
+docker run -d --name ${TAG} -p 80:80 -p 433:433 --rm ${TAG}
 docker image prune -a -f
